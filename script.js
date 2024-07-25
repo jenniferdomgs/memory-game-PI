a45189ae3233db722a0d5db3a86672f107154b11
Jennifer <99359081+jenniferdomgs@users.noreply.github.com>
2024-07-25 20:19:49 -0300

    createBoard + flipCard ok

--- script.js
+++ script.js
@@ -11,14 +11,12 @@ const frutasNome = [
     "Pera"
 ];
 
-// se formos fazer c img
-const frutasImg = [
-
-]
-
 // serve p duplicar a lista dos nomes (ai temos pares)
 let cartas = frutasNome.concat(frutasNome); 
 
+let hasFlippedCard = false;
+let firstCard, secondCard;
+let lockBoard = false;
 
 // serve p embaralhar a lista
 function shuffle(array) {
@@ -31,10 +29,34 @@ function shuffle(array) {
 
 // serve p criar o tabuleiro 
 function createBoard() {
-    const gameBoard = document.getElementById('gameBoard');
-    gameBoard.innerHTML = '';
-    shuffle(cartas); 
+    const gameBoard = document.getElementById('gameBoard'); // pega onde vamos fazer o tabuleiro
+    gameBoard.innerHTML = ''; // limpa o tabuleiro
+    shuffle(cartas); // embaralhando as cartas
     for (let i = 0; i < cartas.length; i++) {
-       
+        const card = document.createElement('div'); // criando as divs das cartas
+        card.classList.add('card'); // p colocar classe na div
+        card.dataset.name = cartas[i]; // p guardar os nomes das frutas
+        card.addEventListener('click', flipCard); // chama a função quando clicar na carta
+        gameBoard.appendChild(card); // add as cartas no tabuleiro (1 por 1)
     }
 }
+
+function flipCard() {
+    if (lockBoard) return; // enquanto as cartas tão desvirando, n dá p virar outra
+    if (this === firstCard) return; // se a carta virada for clicada dnv (nada acontece)
+
+    this.classList.add('flipped'); // add classe p poder mexer no css
+    this.textContent = this.dataset.name; // colocando o nome da fruta como conteúdo
+
+    if (!hasFlippedCard) { // aacontece qndo nenhuma carta foi clicada ainda
+        hasFlippedCard = true;
+        firstCard = this; // guarda a carta atual clicada
+        return; 
+    }
+
+    secondCard = this; // guarda a segunda carta clicada
+    checkForMatch(); // chama a função p ver se as cartas é um par
+}
+
+
+document.addEventListener('DOMContentLoaded', createBoard); // cria o tabueleiro quando a pag é carregada
